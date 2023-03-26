f4eee86d8df800c7669cb4ed51e042e57806df69
Dhammdip Modak <124656648+dhammu10@users.noreply.github.com>
2023-03-26 13:18:26 +0530

    Create EqualNotEqualOperatorsExample.java

---
+++ EqualNotEqualOperatorsExample.java
@@ -0,0 +1,31 @@
+// Program to equal operator and not equal operators
+
+public class EqualNotEqualOperatorsExample {
+
+    public static void main(String[] args) {
+        
+        // Declare and initialize variables
+        int num1 = 5;
+        int num2 = 3;
+        
+        // Compare using the equal operator
+        if (num1 == num2) {
+            System.out.println("num1 is equal to num2");
+        } else {
+            System.out.println("num1 is not equal to num2");
+        }
+        
+        
+        
+        // Compare using the not equal operator
+        if (num1 != num2) {
+            System.out.println("num1 is not equal to num2");
+        } else {
+            System.out.println("num1 is equal to num2");
+        }
+        
+       
+        
+    }
+
+}
